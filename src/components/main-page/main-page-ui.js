6ba1eeaaed90a2164d72230f9c89ec22158a26db
Julian Soro <jsoro@acemetrix.com>
2015-11-29 20:00:55 -0800

    Add coordinates to places

--- src/components/main-page/main-page-ui.js
+++ src/components/main-page/main-page-ui.js
@@ -15,44 +15,154 @@ define(['text!./main-page.html', 'knockout'],
             /*
              * Places model
              */
-            self.places = ko.observableArray( [{"name":"Santa Monica Pier"},
-                {"name":"Santa Monica State Beach"},
-                {"name":"Third Street Promenade"},
-                {"name":"Pacific Park"},
-                {"name":"Palisades Park"},
-                {"name":"Santa Monica Pier Aquarium"},
-                {"name":"Santa Monica Looff Hippodrome"},
-                {"name":"Muscle Beach"},
-                {"name":"Museum of Flying"},
-                {"name":"Santa Monica Museum of Art"},
-                {"name":"Angels Attic"},
-                {"name":"Universal Studios Hollywood"},
-                {"name":"Disneyland"},
-                {"name":"Hollywood Walk of Fame"},
-                {"name":"Griffith Park"},
-                {"name":"Getty Center"},
-                {"name":"Griffith Observatory"},
-                {"name":"TCL Chinese Theatre"},
-                {"name":"Hollywood Sign"},
-                {"name":"Walt Disney Concert Hall"},
-                {"name":"La Brea Tar Pits"},
-                {"name":"Hollywood Bowl"},
-                {"name":"Staples Center"},
-                {"name":"Los Angeles Zoo"},
-                {"name":"Museum of Contemporary Art, Los Angeles"},
-                {"name":"Dolby Theatre"},
-                {"name":"L.A. Live"},
-                {"name":"Getty Villa"},
-                {"name":"Madame Tussauds Hollywood"},
-                {"name":"Universal CityWalk"},
-                {"name":"Runyon Canyon Park"},
-                {"name":"Dodger Stadium"},
-                {"name":"Petersen Automotive Museum"},
-                {"name":"The Grammy Museum"},
-                {"name":"El Capitan Theatre"},
-                {"name":"The Grove at Farmers Market"},
-                {"name":"Greek Theatre (Los Angeles)"}
-            ]);
+            self.places = ko.observableArray( [{
+                    "name":"Santa Monica Pier",
+                    "coords": {lat: 34.0099455, lng: -118.49637860000001}
+                },
+                {
+                    "name":"Santa Monica State Beach",
+                    "coords": {lat: 34.0072324, lng: -118.49369100000001}
+                },
+                {
+                    "name":"Third Street Promenade",
+                    "coords": {lat: 34.01627, lng: -118.49687}
+                },
+                {
+                    "name":"Pacific Park",
+                    "coords": {lat: 34.008456, lng: -118.49748699999998}
+                },
+                {
+                    "name":"Palisades Park",
+                    "coords": {lat: 34.023018, lng: -118.50947859999997}
+                },
+                {
+                    "name":"Santa Monica Pier Aquarium",
+                    "coords": {lat: 34.0104235, lng: -118.4959508}
+                },
+                {
+                    "name":"Santa Monica Looff Hippodrome",
+                    "coords": {lat: 34.0099455, lng: -118.4963786}
+                },
+                {
+                    "name":"Muscle Beach",
+                    "coords": {lat: 33.9854323, lng: -118.47270249999997}
+                },
+                {
+                    "name":"Museum of Flying",
+                    "coords": {lat: 34.0145512, lng: -118.4477099}
+                },
+                {
+                    "name":"Santa Monica Museum of Art",
+                    "coords": {lat: 34.057104, lng: -118.41755639999997}
+                },
+                {
+                    "name":"Angels Attic",
+                    "coords": {lat: 34.014686, lng: -118.49045380000001}
+                },
+                {
+                    "name":"Universal Studios Hollywood",
+                    "coords": {lat: 34.13811680000001, lng: -118.35337830000003}
+                },
+                {
+                    "name":"Disneyland",
+                    "coords": {lat: 33.815458, lng: -117.92358569999999}
+                },
+                {
+                    "name":"Hollywood Walk of Fame",
+                    "coords": {lat: 34.1017458, lng: -118.32719159999999}
+                },
+                {
+                    "name":"Griffith Park",
+                    "coords": {lat: 34.13655440000001, lng: -118.29419999999999}
+                },
+                {
+                    "name":"Getty Center",
+                    "coords": {lat: 34.0780358, lng: -118.47409540000001}
+                },
+                {
+                    "name":"Griffith Observatory",
+                    "coords": {lat: 34.119322, lng: -118.30015200000003}
+                },
+                {
+                    "name":"TCL Chinese Theatre",
+                    "coords": {lat: 34.1020231, lng: -118.34097120000001}
+                },
+                {
+                    "name":"Hollywood Sign",
+                    "coords": {lat: 34.1341151, lng: -118.3215482}
+                },
+                {
+                    "name":"Walt Disney Concert Hall",
+                    "coords": {lat: 34.0554362, lng: -118.24993999999998}
+                },
+                {
+                    "name":"La Brea Tar Pits",
+                    "coords": {lat: 34.0637876, lng: -118.35543369999999}
+                },
+                {
+                    "name":"Hollywood Bowl",
+                    "coords": {lat: 34.112224, lng: -118.3391279}
+                },
+                {
+                    "name":"Staples Center",
+                    "coords": {lat: 34.0430315, lng: -118.26684899999998}
+                },
+                {
+                    "name":"Los Angeles Zoo",
+                    "coords": {lat: 34.1549784, lng: -118.29718739999998}
+                },
+                {
+                    "name":"Museum of Contemporary Art, Los Angeles",
+                    "coords": {lat: 34.0504802, lng: -118.23854080000001}
+                },
+                {
+                    "name":"Dolby Theatre",
+                    "coords": {lat: 34.1026919, lng: -118.34041560000003}
+                },
+                {
+                    "name":"L.A. Live",
+                    "coords": {lat: 34.0447628, lng: -118.26542890000002}
+                },
+                {
+                    "name":"Getty Villa",
+                    "coords": {lat: 34.0452944, lng: -118.56521279999998}
+                },
+                {
+                    "name":"Madame Tussauds Hollywood",
+                    "coords": {lat: 34.101712, lng: -118.34153500000002}
+                },
+                {
+                    "name":"Universal CityWalk",
+                    "coords": {lat: 34.136315, lng: -118.35480100000001}
+                },
+                {
+                    "name":"Runyon Canyon Park",
+                    "coords": {lat: 34.11289229999999, lng: -118.35070669999999}
+                },
+                {
+                    "name":"Dodger Stadium",
+                    "coords": {lat: 34.07273560000001, lng: -118.24061560000001}
+                },
+                {
+                    "name":"Petersen Automotive Museum",
+                    "coords": {lat: 34.062348, lng: -118.36113360000002}
+                },
+                {
+                    "name":"The Grammy Museum",
+                    "coords": {lat: 34.0447868, lng: -118.26529820000002}
+                },
+                {
+                    "name":"El Capitan Theatre",
+                    "coords": {lat: 34.101195, lng: -118.33978000000002}
+                },
+                {
+                    "name":"The Grove at Farmers Market",
+                    "coords": {lat: 34.07142339999999, lng: -118.3577406}
+                },
+                {
+                    "name":"Greek Theatre (Los Angeles)",
+                    "coords": {lat: 34.11950569999999, lng: -118.29633150000001}
+            }]);
 
             /*
              * Clears the search query
