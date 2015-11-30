4c42d0a3fc0b9ba5cbe341107a28d02bf131028b
Julian Soro <jsoro@acemetrix.com>
2015-11-29 20:43:54 -0800

    Update UI as selected place changes

--- src/components/map/map-ui.js
+++ src/components/map/map-ui.js
@@ -28,17 +28,34 @@ define(['text!./map.html', 'knockout', 'jquery'],
                 /** 
                  * Open the place's infoWindow when its marker is clicked
                  */
-                google.maps.event.addListener(place.marker, 'click', (function(iw, places){
+                google.maps.event.addListener(place.marker, 'click', (function(place, selected){
                     return function(){
-                        /** Close all infowindows on the map */
-                        places().forEach(function(place){
-                            place.infowindow.close();
-                        });
-                        /** Open the current infowindow */
-                        iw.open(this.map, this)
+                        /** Set selected place */
+                        selected(place);
                     };
-                })(place.infowindow, self.list));
+                })(place, self.selected));
             });
+
+            /**
+             * Subscribe to the selected place for changes
+             * - focus the map on the marker
+             */
+            self.selected.subscribe((function(places){
+                return function(selectedPlace){
+                    /** Close all infowindows on the map */
+                    places().forEach(function(item){
+                        item.infowindow.close();
+                    });
+
+                    /** If a place is selected, open its infowindow */
+                    if(selectedPlace){
+                        selectedPlace.infowindow.open(selectedPlace.marker.map, selectedPlace.marker);
+                        // Re-center the map
+                        selectedPlace.marker.map.setCenter(selectedPlace.coords);
+                    }
+                };
+            })(self.list));
+
         };
 
         // This runs when the component is torn down. Put here any logic necessary to clean up,
