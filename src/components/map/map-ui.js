9962d12f6a2039e1b68dd583315d25c7c869be6f
Julian Soro <jsoro@acemetrix.com>
2015-11-29 20:03:44 -0800

    Add markers to map

--- src/components/map/map-ui.js
+++ src/components/map/map-ui.js
@@ -1,5 +1,5 @@
-define(['text!./map.html', 'knockout'],
-    function(template, ko) {
+define(['text!./map.html', 'knockout', 'jquery'],
+    function(template, ko, $) {
         'use strict';
 
         var Map = function(params, componentInfo) {
@@ -8,6 +8,29 @@ define(['text!./map.html', 'knockout'],
             self.list = params.list;
             self.selected = params.selected;
 
+            /** Instantiate Google Maps */
+            self.map = window.map;
+
+            /** Create markers for all places */
+            self.list().forEach(function(place){
+                /** Create infoWindows for all places */
+                place.infowindow = new google.maps.InfoWindow({
+                    position: place.coords,
+                    content: '<div><h5>'+place.name+'</h5></div>'
+                });
+
+                // Create a marker for each place
+                place.marker = new google.maps.Marker({
+                    position: place.coords,
+                    map: self.map,
+                    title: place.name
+                });
+
+                /** Connect the place's infoWindow to its marker */
+                place.marker.addListener('click', function(){
+                    place.infowindow.open(self.map);
+                });
+            });
         };
 
         // This runs when the component is torn down. Put here any logic necessary to clean up,
